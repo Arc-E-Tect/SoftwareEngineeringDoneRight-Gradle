4726ad11689e1d07159a97e29dbeac81d96a098d
Iwan Eising <iwan@arc-e-tect.com>
2026-04-11 22:52:36 +0200

    chore: add reusable workflows and sedr-library project files

---
+++ sedr_utils/sedr-library/release.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    branches: ["main"],
+    tagFormat: "sedr-library-v${version}",
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        ["@semantic-release/exec", {
+            prepareCmd: "sed -i.bak 's/^version=.*/version=${nextRelease.version}/' gradle.properties && rm -f gradle.properties.bak"
+        }],
+        ["@semantic-release/changelog", { changelogFile: "CHANGELOG.md" }],
+        ["@semantic-release/git", {
+            assets: ["CHANGELOG.md", "gradle.properties"],
+            message: "chore(release): release ${nextRelease.version} [skip ci]"
+        }],
+        "@semantic-release/github"
+    ]
+};
